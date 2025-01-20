a8d8c4f4e868a3f29aefd6f08656f067b9dbed4b
Jim Fleax <jimfleax@proton.me>
2025-01-20 17:11:03 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
         var count1 = 20;
         setInterval( () => {
             if (crease) {
-                document.querySelector("body").style.background = `linear-gradient(${count1 += 1}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
+                document.querySelector("body").style.background = `linear-gradient(${count1 += 2}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
             } else {
-                document.querySelector("body").style.background = `linear-gradient(${count1 -= 1}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
+                document.querySelector("body").style.background = `linear-gradient(${count1 -= 2}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
             }
             if (count1 === 20) {
                 crease = true;
@@ -14,7 +14,7 @@
                 crease = false;
             }
         }
-        , 100);
+        , 150);
 
         page(1);
         document.querySelectorAll("#pages > span").forEach( (a, b) => {
