3f9de1b0b4ea48f2478f075b25d3095c23551f57
Jim Fleax <jimfleax@proton.me>
2025-02-26 17:34:14 +0530

    Minor adjustments

--- js/main.js
+++ js/main.js
@@ -4,13 +4,13 @@
     var crease = true;
     var count1 = 20;
 
-    document.body.style.transition = "background 500s ease-in-out";
+    document.body.style.transition = "background 10000s ease-in-out";
 
     function animate() {
       if (crease) {
         document.querySelector(
           "body"
-        ).style.background = `linear-gradient(${(count1 += 0.1)}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
+        ).style.background = `linear-gradient(${(count1 += 0.5)}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
       } else {
         document.querySelector(
           "body"
