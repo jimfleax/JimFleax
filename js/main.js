f6784a1eb19b60f2ee041d22b394a97f1a245119
Jim Fleax <jimfleax@proton.me>
2025-02-26 17:37:45 +0530

    Minor adjustments

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
     var crease = true;
     var count1 = 20;
 
-    document.body.style.transition = "background 10000s ease-in-out";
+    document.body.style.transition = "background 20000s ease-in-out";
 
     function animate() {
       if (crease) {
