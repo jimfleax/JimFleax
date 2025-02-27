e3a7e10794a4014667e15116390dd4da7aa5e997
Jim Fleax <jimfleax@proton.me>
2025-02-27 23:04:01 +0530

    Minor fixes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
 ((page) => {
   document.addEventListener("DOMContentLoaded", (e) => {
     localStorage["keybindings"] || (localStorage["keybindings"] = true);
-let worker = new Worker("worker.main.js");
-worker.postMessage(true);
-worker.onerror = function(error) {
-    console.error("Worker Error:", error);
-};
+    var crease = true;
+    var count1 = 20;
+
+    document.body.style.transition = "background 20000s ease-in-out";
+
+    function animate() {
+      if (crease) {
+        document.querySelector(
+          "body"
+        ).style.background = `linear-gradient(${(count1 += 0.5)}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
+      } else {
+        document.querySelector(
+          "body"
+        ).style.background = `linear-gradient(${(count1 -= 0.5)}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
+      }
+      if (count1 === 20) {
+        crease = true;
+      } else if (count1 === 80) {
+        crease = false;
+      }
+      requestAnimationFrame(animate);
+    }
+
+    requestAnimationFrame(animate);
 
     page(1);
     document.querySelector("#pages").addEventListener("click", (e) => {
