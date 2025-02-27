8891d43b76e9d25f712bca8cae923f6abdbf57c1
Jim Fleax <jimfleax@proton.me>
2025-02-27 14:32:38 +0530

    Minor improvements

--- sw-install.js
+++ sw-install.js
@@ -1,15 +1,1 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").then(
-      (registration) => {
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      (error) => {
-        console.log("ServiceWorker registration failed: ", error);
-      }
-    );
-  });
-}
+"serviceWorker" in navigator && window.addEventListener("load",()=>{navigator.serviceWorker.register("/sw.js").then(reg=>console.log("ServiceWorker registration successful with scope: ",reg.scope),err=>console.log("ServiceWorker registration failed: ",err))});
