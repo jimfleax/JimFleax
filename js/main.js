21c19d5a0dc3cafa3dfd0b962a686742dd23170d
Jim Fleax <jimfleax@proton.me>
2025-03-02 01:12:39 +0530

    Performance updates

--- js/main.js
+++ js/main.js
@@ -21,33 +21,6 @@ localStorage.setItem = function (key, value) {
     window.dispatchEvent(new CustomEvent("keybindings-updated", { detail: keybindingEnabled }));
   }
 };
-/*
-
-    var crease = true;
-    var count1 = 20;
-
-    document.body.style.transition = "background 20000s ease-in-out";
-
-    function animate() {
-      if (crease) {
-        document.querySelector(
-          "body"
-        ).style.background = `linear-gradient(${(count1 += 0.5)}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
-      } else {
-        document.querySelector(
-          "body"
-        ).style.background = `linear-gradient(${(count1 -= 0.5)}deg, #1087aa2e, #b4a10f36, white, white, white, #85d0e629, white, white, #e4939314, white, white, #81e44b4a, white, white, #ff8b8b45, #6da7ff40) no-repeat no-repeat`;
-      }
-      if (count1 === 20) {
-        crease = true;
-      } else if (count1 === 80) {
-        crease = false;
-      }
-      requestAnimationFrame(animate);
-    }
-
-    requestAnimationFrame(animate);
-*/
     page(1);
     document.querySelector("#pages").addEventListener("click", (e) => {
       if (e.target.tagName === "SPAN") {
