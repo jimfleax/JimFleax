6b5502149e3b28b45d67444ffd7eae9fa6d95852
Reetabrata Bhandari <jimfleax@proton.me>
2026-01-06 00:29:48 +0530

    feat: Add JavaScript obfuscation plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), obfuscatorPlugin({
